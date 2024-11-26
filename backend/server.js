262bf04bd594dfad79cbfcb56b74dd23a9f31130
op <omprakashreddy.j@gmail.com>
2024-11-26 16:26:52 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express= require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const app=express();
+app.use(cors());
+connectDB();
+app.use(express.json({extended:false}));
+app.get('/',(req,res)=> res.send('API running'));
+// app.use('/api/users',require('./routes/api/users'));
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,()=> console.log(`server started on port ${PORT}`));
